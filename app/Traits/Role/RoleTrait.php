ac36ca416dc45ea54b3a9a086700027d451bd683
DESKTOP-6RABVLR\Jr Flores <smplyjr.dev@gmail.com>
2021-12-20 09:15:35 +0800

    - https://trello.com/c/Ptc2EUZe/138-the-roles-for-admin-and-agent-are-not-displayed - https://trello.com/c/Wnrrnrio/139-no-roles-are-selected-on-admin-and-agent-roles-when-editing-it - https://trello.com/c/rQO025gE/137-invalid-error-message-appears-when-accessing-the-settings-roles - https://trello.com/c/y2gphsYp/135-cannot-add-edit-roles-in-settings

--- app/Traits/Role/RoleTrait.php
+++ app/Traits/Role/RoleTrait.php
@@ -9,7 +9,7 @@ trait RoleTrait
     public function datatable()
     {
         $query = cache()->remember('roles-index', remember_for(5), function () {
-            return Role::with('permissions')->whereNotIn('id', [1]);
+            return Role::with('permissions')->whereNotIn('id', [1])->get();
         });
 
         return response()->json($query, 200);
