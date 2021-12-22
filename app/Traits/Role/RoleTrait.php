576ed9ec459370c817cad9e026fd10e58fb3501f
DESKTOP-6RABVLR\Jr Flores <smplyjr.dev@gmail.com>
2021-12-22 08:33:27 +0800

    - https://trello.com/c/y2gphsYp/135-cannot-add-edit-roles-in-settings

--- app/Traits/Role/RoleTrait.php
+++ app/Traits/Role/RoleTrait.php
@@ -9,7 +9,21 @@ trait RoleTrait
     public function datatable()
     {
         $query = cache()->remember('roles-index', remember_for(5), function () {
-            return Role::with('permissions')->whereNotIn('id', [1])->get();
+            return Role::with('permissions')->whereNotIn('id', [1])
+                ->get()
+                ->map(function ($post) {
+                    return [
+                        'id'          => $post->id,
+                        'name'        => $post->name,
+                        'permissions' => $post->permissions->map(function ($permission) {
+                            return [
+                                'name' => $permission->name,
+                                'slug' => $permission->slug
+                            ];
+                        })
+                    ];
+                })
+                ->toArray();
         });
 
         return response()->json($query, 200);
